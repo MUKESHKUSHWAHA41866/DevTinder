d2fa4aaf62e83ed1f287cced8db74b98cc85eb21
Mukesh Kushwaha <singhmukesh41866@gmail.com>
2024-12-05 11:32:44 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@
 
  const app = express();
 
- app.get("/user/:userId/:name/:password",(req, res)=>{
-   console.log(req.params);
+//  app.get("/user/:userId/:name/:password",(req, res)=>{
+//    console.log(req.params);
    
-   res.send({firstName: "Mukesh",lastName: "kushwaha"})
- })
+//    res.send({firstName: "Mukesh",lastName: "kushwaha"})
+//  })
+
+// app.use("/routes", rH, [rh2, rh3], rh4, rh5);
+
+app.use("/user", (req,res,next) =>{
+  console.log("Handleing the route user!!");
+  next();
   
+},
+(req,res,next)=>{
+  console.log("Handling 2 route");
+  next();
+},
+(req,res,next)=>{
+  console.log("Handleing 3 routes");
+  res.send("Hello mukesh")
+})
   
 
  app.listen(777,()=> {
