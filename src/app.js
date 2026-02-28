11df85b7aa7de9c2cef21b5df399471949002ad0
Mukesh Kushwaha <singhmukesh41866@gmail.com>
2026-02-28 23:57:18 +0530

    add real time chat feature

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors")
+const http = require("http");
 require('dotenv').config();
 
 require("./utils/cronjob.js");
@@ -31,18 +32,24 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 const paymentRouter = require("./routes/payment.js");
+const initializeSocket = require("./utils/socket.js");
+const chatRouter = require("./routes/chat.js");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
- 
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
+
  connectDB()
   .then(()=>{
     console.log("Database connection established...");
 
-    app.listen(process.env.PORT,()=> {
+    server.listen(process.env.PORT,()=> {
       console.log("Server is successfully listing on port 7777...");
       
    });
